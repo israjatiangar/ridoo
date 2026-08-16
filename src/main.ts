0a044661069da9bae73ce7a681b1c303b555ba9d
Rajat Iangar <10295339+israjatiangar@users.noreply.github.com>
2026-08-16 13:44:42 +0530

    style(index): add more icons
    ---
    style(index): use fluenticons for all
    del(main.ts): del dynamic theme icon

--- src/main.ts
+++ src/main.ts
@@ -66,18 +66,7 @@ function addListItem(task: Task): void {
 }
 
 themeButton?.addEventListener("click", () => {
-	let theme = toggleTheme()
-	if (theme === null) {
-		return
-	}
-	if (theme === "light") {
-		themeButton.innerHTML = `<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" >
-  		<path d="M10 2a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0v-1.5A.75.75 0 0 1 10 2ZM10 15a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0v-1.5A.75.75 0 0 1 10 15ZM10 7a3 3 0 1 0 0 6 3 3 0 0 0 0-6ZM15.657 5.404a.75.75 0 1 0-1.06-1.06l-1.061 1.06a.75.75 0 0 0 1.06 1.06l1.06-1.06ZM6.464 14.596a.75.75 0 1 0-1.06-1.06l-1.06 1.06a.75.75 0 0 0 1.06 1.06l1.06-1.06ZM18 10a.75.75 0 0 1-.75.75h-1.5a.75.75 0 0 1 0-1.5h1.5A.75.75 0 0 1 18 10ZM5 10a.75.75 0 0 1-.75.75h-1.5a.75.75 0 0 1 0-1.5h1.5A.75.75 0 0 1 5 10ZM14.596 15.657a.75.75 0 0 0 1.06-1.06l-1.06-1.061a.75.75 0 1 0-1.06 1.06l1.06 1.06ZM5.404 6.464a.75.75 0 0 0 1.06-1.06l-1.06-1.06a.75.75 0 1 0-1.061 1.06l1.06 1.06Z" /></svg>`
-	} else {
-		themeButton.innerHTML = `
-		<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
-		<path d="M14.438 10.148c.19-.425-.321-.787-.748-.601A5.5 5.5 0 0 1 6.453 2.31c.186-.427-.176-.938-.6-.748a6.501 6.501 0 1 0 8.585 8.586Z" /></svg>`
-	}
+	toggleTheme()
 })
 
 clearAllButton?.addEventListener("click", () => {
@@ -87,14 +76,13 @@ clearAllButton?.addEventListener("click", () => {
 	saveTasks()
 })
 
-function toggleTheme(): string {
+function toggleTheme(): void {
 	let theme = document.querySelector("html")?.getAttribute("data-theme")
 	if (theme === "dark") {
 		document.querySelector("html")?.setAttribute("data-theme", "light")
 	} else {
 		document.querySelector("html")?.setAttribute("data-theme", "dark")
 	}
-	return theme ?? "dark"
 }
 
 function saveTasks(): void {
